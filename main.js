13ce68d127f7d7cbf2e3d2a6182caba59f5db1fc
Raffisa17 <mochammadraffi885@gmail.com>
2025-12-14 13:53:15 +0700

    feat: money logic

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const input = require('prompt-sync')({sigint: true});
 
-const umur = Number(input("Masukkan umur Anda:"));
 const nama = input("Masukkan nama Anda:");
+const umur = Number(input("Masukkan umur Anda:"));
 
 if (umur < 21) {
-    console.log("Maaf " + nama + ", umur kamu kurang " + (21 - umur) + " lagi");
-} else if (umur >= 21) {
-    console.log("Seberapa banyak uang kamu anak muda?");
+    console.log("Maaf " + nama + ", umur kamu kurang " + (21 - umur) + " tahun lagi");
 } else {
-    console.log("Selamat datang " + nama);
+    console.log("Seberapa banyak uang kamu anak muda?");
+    const uang = Number(input("Masukkan jumlah uang Anda:"));
+
+    if (uang < 500000) {
+            console.log("Maaf " + nama + ", uang kamu cuma " + (uang) + " datang lagi lain kali");
+        } else {
+            console.log("Selamat datang " + nama);
+        }
 }
